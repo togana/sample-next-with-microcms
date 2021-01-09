c05364e9e5f6a413e62ae78cd12e306b819cd658
togana <o.togana@gmail.com>
2021-01-09 14:15:04 +0900

    フラグメントでくくるの忘れてた

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,10 +1,12 @@
 export const Blog = (blog) => (
-  <h1>{blog.title}</h1>
-  <p>{blog.publishedAt}</p>
-  <p>{blog.category && `${blog.category.name}`}</p>
-  <div
-    dangerouslySetInnerHTML={{
-      __html: `${blog.body}`,
-    }}
-  />
+  <>
+    <h1>{blog.title}</h1>
+    <p>{blog.publishedAt}</p>
+    <p>{blog.category && `${blog.category.name}`}</p>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `${blog.body}`,
+      }}
+    />
+  </>
 );
