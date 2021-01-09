94e61e22727b7cd475f4374fea5e1b86f36d5945
togana <o.togana@gmail.com>
2021-01-09 12:19:19 +0900

    カテゴリー追加した

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -3,6 +3,7 @@ export default function BlogBody({ blog }) {
     <main>
       <h1>{blog.title}</h1>
       <p>{blog.publishedAt}</p>
+      <p>{blog.category && `${blog.category.name}`}</p>
       <div
         dangerouslySetInnerHTML={{
           __html: `${blog.body}`,
